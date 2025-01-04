2d012b0f3e64ade56a5591cb800a4d0aca497fd6
Marcelo de Bittencourt <4615986+marcelodebittencourt@users.noreply.github.com>
2025-01-04 19:24:51 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi doguinha, te amo. Se me ama, dá uma risadinha! :) :)</h1>
+  return <h1>Oi doguinha, te amo. Se me ama, dá uma risadinha! :) :)</h1>;
 }
 
 export default Home;
